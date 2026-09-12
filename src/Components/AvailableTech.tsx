9f766c841240f67a6c0323522d18cad70617c442
MrAbiduuu <MrAbiduuu>
2026-09-12 12:42:07 +0600

    Added stack beside

--- src/Components/AvailableTech.tsx
+++ src/Components/AvailableTech.tsx
@@ -4,31 +4,40 @@ import { FaStar } from "react-icons/fa";
 
 const AvailableTech = ({ Technology }) => {
   return (
-    <div className="mx-auto grid grid-cols-3 min-h-150 max-w-7xl items-center px-6 py-16 lg:px-8 gap-2">
-      {Technology.map((Tech: dataTypes) => (
-        <div className="p-5 border-1 rounded-2xl">
-          <div className="flex justify-between">
-            <img src={Tech.icon} alt={Tech.name} />
-            <p>{Tech.type}</p>
-          </div>
-          <h1 className="text-2xl font-bold">{Tech.name}</h1>
-          <p>{Tech.description}</p>
-          <hr className="text-gray-400" />
-          <div className="flex justify-between">
-            <p>{Tech.category}</p>
-            <p>{Tech.environment}</p>
-            <p className="flex text-center align-bottom">
-              <FaStar className="text-[#FBBF24]" />
-              {Tech.rating}
-            </p>
-          </div>
-          <div className="text-center">
-            <button className="btn btn-active bg-black text-white rounded-field w-full">
-              Add to stack
-            </button>
-          </div>
+    <div className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-6 lg:grid-cols-4">
+      <div className="lg:col-span-3">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {Technology.map((Tech: dataTypes) => (
+            <div className="p-5 border rounded-2xl">
+              <div className="flex justify-between">
+                <img src={Tech.icon} alt={Tech.name} />
+                <p>{Tech.type}</p>
+              </div>
+              <h1 className="text-2xl font-bold">{Tech.name}</h1>
+              <p>{Tech.description}</p>
+              <hr className="text-gray-400" />
+              <div className="flex justify-between">
+                <p>{Tech.category}</p>
+                <p>{Tech.environment}</p>
+                <p className="flex text-center align-bottom">
+                  <FaStar className="text-[#FBBF24]" />
+                  {Tech.rating}
+                </p>
+              </div>
+              <div className="text-center">
+                <button className="btn btn-active bg-black text-white rounded-field w-full">
+                  Add to stack
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
+      <div className="lg:col-span-1">
+        <div className="rounded-2xl border p-6">
+          <h2 className="text-2xl font-bold">My Stack</h2>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Technologies.tsx
+++ src/Components/Technologies.tsx
@@ -10,7 +10,16 @@ const Technologies = ({ TechPromise }: TechProps) => {
   const Technology = use(TechPromise);
   console.log(Technology);
   return (
-    <div>
+    <div className="mx-auto min-h-150 max-w-7xl items-center px-6 py-16 grid-cols-1">
+      <h1 className="font-bold text-3xl">
+        Explore the
+        <span className="font-bold bg-linear-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent ml-2">
+          Technologies
+        </span>
+      </h1>
+      <p className="text-gray-400">
+        Pick one technolog per category to build your ideal stack
+      </p>
       <AvailableTech Technology={Technology} />
     </div>
   );
